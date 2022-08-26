b468a3ea007bd26f39e7b9d839e1ebe279b939e1
Ayo <ayokunz@gmail.com>
2022-08-26 18:11:19 +0100

    added project section

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 let ham = document.querySelector('.ham');
 let hamimg = document.querySelector('.ham img');
 let nav = document.querySelector('#nav-menu');
+let mainnav = document.querySelector("nav");
+
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY >= 100){
+            mainnav.classList.add("nav-container-bg");
+        }
+        else{
+            mainnav.classList.remove("nav-container-bg");
+
+        }
+    }     
+)
 
 ham.addEventListener('click' , ()=>{
     if(nav.className == "nav-menu"){
